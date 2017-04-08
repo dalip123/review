ce5760ec6119451abbe271958bc183c3715894ba
dalip123 <dalipkumar703@gmail.com>
2017-04-08 20:39:55 +0530

    google api

--- config/services.php
+++ config/services.php
@@ -38,12 +38,12 @@ return [
     'facebook' => [
         'client_id'     => '1351808858219638',
         'client_secret' => 'c0ab4e1dd86dcb91eff660a40fc96151',
-        'redirect'      => 'http://localhost:8000/social/login/facebook',
+        'redirect'      => 'http://glide.ezmart.in/social/login/facebook',
     ],
     'google'=> [
-       'client_id'     => '1351808858219638',
-        'client_secret' => 'c0ab4e1dd86dcb91eff660a40fc96151',
-        'redirect'      => 'http://localhost:8000/social/login/google',
+       'client_id'     => '1068463228284-t6a2dpugsim8ca0p2bbeip7et5g3ng2g.apps.googleusercontent.com',
+        'client_secret' => '6MkGFwDPNIP7oLrLyMSLEij6',
+        'redirect'      => 'http://glide.ezmart.in/social/login/google',
     ]
 ];
  
