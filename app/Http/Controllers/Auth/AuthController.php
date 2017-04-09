e5e1ac0cf82f9f12ddf064e4b1ea894608b4b7fd
dalip123 <dalipkumar703@gmail.com>
2017-04-09 18:55:51 +0530

    review appication

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -89,7 +89,7 @@ class AuthController extends Controller
        if(!empty($namecheck))
        $reviewActive=true;
        
-       return redirect('/')->with('reviewActive',$reviewActive);
+       return redirect()->back();
        
     }
 }
