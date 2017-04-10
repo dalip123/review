ad8fa04aa959434e048506845fa20d9e82367d89
dalip123 <dalipkumar703@gmail.com>
2017-04-10 22:39:41 +0530

    review appication

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,14 +6,15 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   <script src="public/js/index.js"></script>
   <script src="public/app/app.js"></script>
     </head>
     <body ng-app="myApp">
-<div class="container" ng-controller="indexcontroller">
+<div class="container" >
 <div class="text-centre"><h2>Give us Feedback</h2></div>
-<form action="" class="form-horizontal" >
+<form action="" class="form-horizontal" ng-controller="indexcontroller">
 <div class="form-group">
 <label for="name">Name:</label>
 <input type="text" class="form-control" id="name" name="name">
