5f40534ff137f84b018b207209a35175601db103
dalip123 <dalipkumar703@gmail.com>
2017-04-10 22:44:25 +0530

    review appication

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,6 @@
  <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   <script src="public/js/index.js"></script>
@@ -16,7 +15,7 @@
 <div class="text-centre"><h2>Give us Feedback</h2></div>
 <form action="" class="form-horizontal" ng-controller="indexcontroller">
 <div class="form-group">
-<label for="name">Name:</label>
+<label for="name">Name: <% countFacebookLink %></label>
 <input type="text" class="form-control" id="name" name="name">
 </div>
 <div class="form-group">
